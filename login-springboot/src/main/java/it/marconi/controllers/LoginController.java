6c5a6ceba00a7a3a21aac3023c6ffa6e7aea4f9b
SaraScattone <sara.scattone@marconirovereto.it>
2024-05-05 01:44:22 +0200

    Errore su metodo get in LoginService

--- login-springboot/src/main/java/it/marconi/controllers/LoginController.java
+++ login-springboot/src/main/java/it/marconi/controllers/LoginController.java
@@ -1,13 +1,22 @@
 package it.marconi.controllers;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import it.marconi.domains.Login;
+import it.marconi.domains.LoginForm;
 import it.marconi.services.LoginService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/login")
@@ -30,4 +39,24 @@ public class LoginController {
 
         return modelAndView;
     }
+
+    @PostMapping("/utente")
+    public ModelAndView visualizzaUtente(@ModelAttribute @Valid LoginForm loginForm,
+    BindingResult br) {
+
+        // verifico la presenza di errori di validazione, e ricarico la pagina
+        if (br.hasErrors())
+            return new ModelAndView("contact-form");
+
+        Login login = loginService.save(loginForm);
+
+        // applico pattern PRG (Post Redirect Get)
+        return new ModelAndView("redirect:/utente?id=" + login.getId()); 
+    }
+
+    /*@GetMapping(params = "id")
+    public ModelAndView mostraUtente(@RequestParam("id") UUID loginId) {
+
+        Optional<Login> opLogin = loginService.get(loginId);
+    }*/
 }
--- login-springboot/src/main/java/it/marconi/services/LoginService.java
+++ login-springboot/src/main/java/it/marconi/services/LoginService.java
@@ -1,5 +1,38 @@
 package it.marconi.services;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.marconi.domains.Login;
+import it.marconi.domains.LoginForm;
+import it.marconi.repositories.LoginRepository;
+
+@Service
 public class LoginService {
     
+    @Autowired
+    private LoginRepository loginRepo;
+
+    public Login save(LoginForm loginForm) {
+
+        Login l = mapLogin(loginForm);
+        return loginRepo.save(l);
+    }
+
+    private Login mapLogin(LoginForm loginForm) {
+
+        Login l = new Login();
+        l.setNickname(loginForm.getNickname());
+        l.setEmail(loginForm.getEmail());
+        l.setPassword(loginForm.getPassword());
+
+        return l;
+    }
+
+    /*public Optional<Login> get(UUID loginId) {
+        return loginRepo.findById(loginId);
+    }*/
 }
